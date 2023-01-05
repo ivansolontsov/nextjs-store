2736cf239ffcbb286bde632839edd9e95293d86c
Ivan Solontsov <ivansolontsov@yandex.ru>
2023-01-05 04:31:57 +0400

    active links for header, css fixes for hero component

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,15 +6,17 @@ import Link from 'next/link';
 import CartButton from './actionButtons/CartButton';
 import SearchButton from './actionButtons/SearchButton';
 import Search from './Search';
+import { useRouter } from 'next/router';
 
 const Header: React.FC = () => {
+    const { query, pathname } = useRouter()
     return (
         <header className={`header`}>
             <div className="header__left-side">
                 <Link href='/'><Image priority={true} src={logo} width={247} height={48} alt={'logo'} /></Link>
                 <nav className='header__nav'>
                     {navRoutes.map((route, index) => (
-                        <Link key={index} href={route.path} className='header__nav-link'>{route.name}</Link>
+                        <Link key={index} href={route.path} className={`header__nav-link ${pathname === route.path ? 'header__nav-link_active' : ''}`}>{route.name}</Link>
                     ))}
                 </nav>
             </div>
