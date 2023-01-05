8c1c94f1428eb73c1e3774bd6568a30019bd2b4e
Ivan Solontsov <ivansolontsov@yandex.ru>
2023-01-05 04:17:33 +0400

    scroll lock when preloader is active

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,11 +11,16 @@ import { wrapper } from '../store/store';
 import Router from 'next/router'
 import { useState, useEffect } from 'react';
 import PreLoader from '../components/PreLoader/PreLoader';
+import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
 
 let persistor = persistStore(store)
 
 export function App({ Component, pageProps }: AppProps) {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    isLoading ? disableBodyScroll(document.body) : enableBodyScroll(document.body)
+  }, [isLoading])
 
   useEffect(() => {
     setIsLoading(false)
